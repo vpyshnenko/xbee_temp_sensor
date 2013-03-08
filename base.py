71a12fd9a37ba4ac8ac73ab4b84c9a436fc11bb2
Vadim Zaliva <lord@yambo.home.crocodile.org>
2013-03-08 12:04:31 -0800

    removing external logger functionality

--- base.py
+++ base.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 #
 #
 
@@ -41,29 +41,6 @@ def cleanup():
         global_lock.release()
 
 
-def send_nimbits(name, value):
-    assert isinstance(value, numbers.Real)
-
-    # create dictionary
-    data = {"email":NIMBITS_EMAIL,
-            "secret":NIMBITS_SECRET,
-            "point":name,
-            "value":'{0:.3f}'.format(value)}
-
-    headers = {"Content-type": "application/x-www-form-urlencoded",
-               "Accept": "text/plain"}
-    try:
-        conn = httplib.HTTPConnection("app.nimbits.com")
-
-        conn.request("POST", "/service/currentvalue",
-                     urllib.urlencode(data), headers)
-
-        response = conn.getresponse()
-        logger.info('Nimbits data point "%s": response: %s %s' % (
-                name, response.status, response.reason))
-    except socket.error, e:
-        logger.error('%s: nimbits socket error: %s' % (datetime.datetime.now(), e))
-
 def record_battery_v(battery_file, value):
     assert isinstance(value, numbers.Real)
     now = datetime.datetime.now()
@@ -75,9 +52,8 @@ def record_battery_v(battery_file, value):
 
 def usage():
     print """
-%s [-n] [-s /dev/ttyS0]
+%s [-s /dev/ttyS0]
 
--n      -- do not send data to nimbits (by default, send)
 -s port -- use serial port <port>. Default is /dev/ttyS0
 
 """  % sys.argv[0]
@@ -97,7 +73,7 @@ def main():
     try:
 
         try:
-            opts, args = getopt.getopt(sys.argv[1:], 's:nc', [])
+            opts, args = getopt.getopt(sys.argv[1:], 's:c', [])
 
         except getopt.GetoptError:
             usage()
@@ -114,14 +90,11 @@ def main():
         atexit.register(cleanup)
 
         serial_port= '/dev/ttyS0'
-        send_to_data_logger = True
         console = False
 
         for o, a in opts:
             if o in ['-s']:
                 serial_port = a
-            elif o in ['-n']:
-                send_to_data_logger = False
             elif o in ['-c']:
                 console = True
             else:
@@ -129,7 +102,6 @@ def main():
                 sys.exit(1)
 
         print 'Using serial port %s' % serial_port
-        print 'Sending to data_logger: %s' % send_to_data_logger
 
         s = serial.serialposix.Serial(port=serial_port,
                                       baudrate=9600, bytesize=8, parity='N', stopbits=1,
