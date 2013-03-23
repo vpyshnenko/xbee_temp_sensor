69d933466f1aa0a97f466a84b4f6e6972480f2c1
Vadim Zaliva <lord@crocodile.org>
2013-03-23 10:33:03 -0700

    removing outliers

--- processing/normalize.R
+++ processing/normalize.R
@@ -1,5 +1,16 @@
 library(signal)
 
+# Remove outliers from a series of observations.
+# We use Chauvenet's criterion to remove outlier rows based on observation
+# column values.
+remove_outliers <- function(xy,c) {
+  m<-mean(xy[,c])
+  s<-sd(xy[,c])
+  n<-nrow(xy)
+  v=dnorm(xy[,c],m,s)*n
+  xy[v>=0.5,]
+}
+
 # Load raw datasets
 data_collector_raw <- read.csv("../data_collector.csv", header=F)
 radiothermostat_raw <- read.csv("../radiothermostat.csv", header=F)
@@ -7,12 +18,12 @@ wu_raw <- read.csv("../wu.csv", header=F)
 
 all_series = list()
 # for Weather Underground, use reported observation time instead of API query time, as they may differ.
-all_series$wu <- wu_raw[,2-3]
+all_series$wu <- remove_outliers(wu_raw[,2-3],2)
 colnames(all_series$wu) <- c("ts","wu_t")
 rm(wu_raw)
 
 # no processing for radio thermostat time
-all_series$radiothermostat <- radiothermostat_raw
+all_series$radiothermostat <- remove_outliers(radiothermostat_raw,2)
 colnames(all_series$radiothermostat) <- c("ts","rs_t","rs_h","rs_f")
 rm(radiothermostat_raw)
 
@@ -21,8 +32,9 @@ for(s in sensors_ids) {
   if(s==0)
     continue; # ID0 is reserved for server
   sname <- paste("sensor",s,sep="");
-  all_series[[sname]] <-
-    cbind(data_collector_raw[data_collector_raw[2]==s,1],data_collector_raw[data_collector_raw[2]==s,5]);
+  all_series[[sname]] <-remove_outliers(
+    cbind(data_collector_raw[data_collector_raw[2]==s,1],data_collector_raw[data_collector_raw[2]==s,5]),
+    2)
   colnames(all_series[[sname]]) <- c("ts",paste("sensor",s,"_t",sep=""))
 }
 rm(s,sname)
