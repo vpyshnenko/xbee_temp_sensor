3aff2d68dce3799bb19ea6c6807db9effdb4594c
Vadim Zaliva <lord@crocodile.org>
2013-04-26 16:33:47 -0700

    plots

--- processing/MRF_learn.m
+++ processing/MRF_learn.m
@@ -61,18 +61,48 @@ w = minFunc(@UGM_MRF_NLL,w,moptions,nInstances,suffStat, ...
 % Get all of the unary and pairwise marginals 
 % (as well as the logarithm of the normalizing constant Z) using:
 
-[nodeBel,edgeBel,logZ] = UGM_Infer_Exact(nodePot,edgePot,edgeStruct)
+%[nodeBel,edgeBel,logZ] = UGM_Infer_Exact(nodePot,edgePot,edgeStruct)
 
 % Optimal decoding
 optimalDecoding = UGM_Decode_Exact(nodePot,edgePot,edgeStruct)
 
-% Clamped decoding
-clamped = zeros(nNodes,1);
-clamped(1) = 2; % Heater ON
-%[nodeBel,edgeBel,logZ] = UGM_Infer_Conditional(nodePot,edgePot, ...
-%    edgeStruct,clamped,@UGM_Infer_Exact);
-edgeStruct.maxIter=1000;
-samples = UGM_Sample_Conditional(nodePot,edgePot, ...
-    edgeStruct,clamped,@UGM_Sample_Exact);
+% Clamped samping
+%clamped = zeros(nNodes,1);
+%clamped(1) = 2; % Heater ON
+%edgeStruct.maxIter=1000;
+%samples = UGM_Sample_Conditional(nodePot,edgePot, ...
+%    edgeStruct,clamped,@UGM_Sample_Exact);
 
+
+% Plot optimal decoding for nodes states
+a = 2;
+b = 3;
+clamped = zeros(nNodes,1);
+x = zeros(nStates(a),1);
+y = zeros(nStates(a),1);
+y1 = zeros(nStates(a),1);
+for i=1:nStates(a)
+    x(i) = i;
+    clamped(a) = i;
+    % Heater ON
+    clamped(1) = 2; 
+    v = UGM_Decode_Conditional(nodePot,edgePot,edgeStruct, ...
+        clamped,@UGM_Decode_Tree);
+    y(i) = v(b);
+    
+    % Heater OFF
+    clamped(1) = 1; 
+    v = UGM_Decode_Conditional(nodePot,edgePot,edgeStruct, ...
+        clamped,@UGM_Decode_Tree);
+    y1(i) = v(b);
+    
+end
+plot(x,y,'r');
+hold on;
+p=plot(x,y1,'g');
+xlabel(a)
+ylabel(b)
+
+
+saveas(p,sprintf('results/%d-%d.png',a,b)) 
 
