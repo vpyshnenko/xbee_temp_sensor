60e2cd50be23abb2cdd7a2b79a99db255a9543eb
Vadim Zaliva <lord@crocodile.org>
2013-04-26 18:03:40 -0700

    final plots based on MRF

--- processing/MRF_learn.m
+++ processing/MRF_learn.m
@@ -76,37 +76,15 @@ optimalDecoding = UGM_Decode_Exact(nodePot,edgePot,edgeStruct)
 
 
 % Plot optimal decoding for nodes states
-a = 2;
-b = 3;
-clamped = zeros(nNodes,1);
-x = unique(y(:,a));
-xsize = size(x,1);
-y0 = zeros(xsize,1);
-y1 = zeros(xsize,1);
-for i=1:xsize
-    clamped(a) = x(i);
-    disp(x(i))
-    % Heater ON
-    clamped(1) = 2; 
-    v = UGM_Decode_Conditional(nodePot,edgePot,edgeStruct, ...
-        clamped,@UGM_Decode_Tree);
-    y0(i) = v(b);
-    
-    % Heater OFF
-    clamped(1) = 1; 
-    v = UGM_Decode_Conditional(nodePot,edgePot,edgeStruct, ...
-        clamped,@UGM_Decode_Tree);
-    y1(i) = v(b);
-    
-end
-mina=min(unique(y(:,[a b])))
-maxa=max(unique(y(:,[a b])))
-plot(x,y0,'r');
-axis([ mina maxa mina maxa ])
-hold on;
-p=plot(x,y1,'g');
-xlabel(a)
-ylabel(b)
+figure;
+p = tempplot(2,3,nNodes, y, nodePot, edgePot, edgeStruct);
+saveas(p,sprintf('results/%d-%d.png',2,3));
 
-saveas(p,sprintf('results/%d-%d.png',a,b)) 
+figure;
+p = tempplot(2,4,nNodes, y, nodePot, edgePot, edgeStruct);
+saveas(p,sprintf('results/%d-%d.png',2,4));
+
+figure;
+p = tempplot(3,4,nNodes, y, nodePot, edgePot, edgeStruct);
+saveas(p,sprintf('results/%d-%d.png',3,4));
 
---
+++ processing/tempplot.m
@@ -0,0 +1,35 @@
+function [ p ] = tempplot( a, b, nNodes, y, nodePot, edgePot, edgeStruct)
+%UNTITLED Plot two temperatures plots
+clamped = zeros(nNodes,1);
+x = unique(y(:,a));
+xsize = size(x,1);
+y0 = zeros(xsize,1);
+y1 = zeros(xsize,1);
+for i=1:xsize
+    clamped(a) = x(i);    
+    % Heater ON
+    clamped(1) = 2; 
+    v = UGM_Decode_Conditional(nodePot,edgePot,edgeStruct, ...
+        clamped,@UGM_Decode_Tree);
+    y0(i) = v(b);
+    
+    % Heater OFF
+    clamped(1) = 1; 
+    v = UGM_Decode_Conditional(nodePot,edgePot,edgeStruct, ...
+        clamped,@UGM_Decode_Tree);
+    y1(i) = v(b);
+    
+end
+mina=min(unique(y(:,[a b])));
+maxa=max(unique(y(:,[a b])));
+plot(x,y0,'red', 'LineWidth', 3);
+axis([ mina maxa mina maxa ]);
+hold on;
+plot(x,y1,'green');
+hold on;
+p=plot(mina:maxa,mina:maxa,'-.k');
+xlabel(a);
+ylabel(b);
+
+end
+
